767ffba0cfb01186bb046e17b9adb69cc3d2e2ec
Ubuntu <hekra01@yahoo.com>
2016-12-01 15:42:33 +0000

    fix startup scripts

--- startup.sh
+++ startup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-export PATH=$PATH:$HOME/RTC/depot_tools
+export PATH=$PATH:/home/ubuntu/RTC/depot_tools
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export PATH=$PATH:/usr/local/go/bin
-export GOPATH=$HOME/RTC/GO
-export PATH=$PATH:$HOME/google-cloud-sdk/bin/:$HOME/google-cloud-sdk/platform/google_appengine
-export PATH=$PATH:/usr/lib/google-cloud-sdk/bin:/usr/local/go/bin:$HOME/RTC/depot_tools
-export ROOM_DIR=$HOME/RTC/apprtc
+export PATH=$PATH:/home/ubuntu/google-cloud-sdk/bin/:/home/ubuntu/google-cloud-sdk/platform/google_appengine
+export PATH=$PATH:/usr/lib/google-cloud-sdk/bin:/usr/local/go/bin:/home/ubuntu/RTC/depot_tools
+export ROOM_DIR=/home/ubuntu/RTC/apprtc
 cd $ROOM_DIR &&dev_appserver.py --host 0.0.0.0 --port 8084 --api_host localhost --api_port 8081 --admin_host localhost --admin_port 8082 out/app_engine/
-
